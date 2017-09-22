4de0ea26f513c33fd6a95652d7ac3d974e9a462b
Miika Koskela <miika.koskela@pp3.inet.fi>
2017-09-22 19:47:24 +0300

    Allow replacing DOMXpath

--- src/MARCXML/Xml/Record.php
+++ src/MARCXML/Xml/Record.php
@@ -3,25 +3,54 @@
 namespace PIKI\MARCXML\Xml;
 
 use DOMDocument;
+use DOMNodeList;
+use DOMNode;
 use DOMXpath;
 
 class Record extends DOMDocument
 {
+    /**
+     * An instance of DOMXPath
+     *
+     * @var DOMXPath
+     */
     private $xpath;
 
+    // Constructor
     public function __construct(string $xml)
     {
         $this->loadXML($xml);
         $this->xpath = new DOMXpath($this);
     }
 
-    public function registerNamespace(string $namespace)
+    /**
+     * Allow replacing DOMXpath.
+     *
+     * This just uses default DOMXPath without any configuration. But if user
+     * wants to, for example, register namespaces and use different queries,
+     * this is possible then.
+     *
+     * @param DOMXPath
+     * @return Record
+     */
+    public function setXPath(DOMXpath $xpath) : self
     {
-        # code...
+        $this->xpath = $xpath;
+        return $this;
     }
 
-    public function query(string $query) : DOMNodeList
+    /**
+     * Query as a wrapper for DOMXPath-member.
+     *
+     * See DOMXPath documentation for more.
+     *
+     * @param string $query
+     * @param DOMNode $context
+     * @param bool $registerNodeNS
+     * @return DOMNodeList
+     */
+    public function query(string $query, DOMNode $context = null, bool $registerNodeNS = true) : DOMNodeList
     {
-        return $this->xpath($query);
+        return $this->xpath->query($query, $context, $registerNodeNS);
     }
 }
